626169171c5d31b2c334f1d0a2fb99d2e404549a
Box <lashawnjsimpson05@gmail.com>
2018-05-22 18:02:05 -0400

    Testing if

---
+++ if-test/if-hostname.py
@@ -0,0 +1,9 @@
+#!/use/bin/env python3
+# Illustrated by Lashawn Simpson
+
+hostname = 'MTG'
+if hostname == 'MTG' :
+	print('The hostname was found to be mtg')
+hostname = input('What is your hostname?')
+if hostname == 'mtg' ',' 'MTG'',' 'mTg' ',' 'MTg' ',' 'mTG' ',' 'MtG' :
+	print('Hostname is correct')
