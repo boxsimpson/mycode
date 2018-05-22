678fa5902655b0854fcaf3a14f073471a30c8aa6
Box <lashawnjsimpson05@gmail.com>
2018-05-22 12:09:15 -0400

    Finished Morning Lab

---
+++ day2/ipask.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Illistrated by Lashawn Simpson
+# Storing IP and MAC addresses in a var
+
+print('What is your IP address?')
+sip = input()
+print('So this your IP address: ' + sip + ' ?')
+
+
+print('What is your MAC address?')
+smac = input()
+print('So your MAC address is: ' + smac + ' ?')
+
+print('Your MAC ' + smac + ' and Your IP ' + sip )
