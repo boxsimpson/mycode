4bf31ca281bb0a01a7cc5158066c9f196372a1ff
Box <lashawnjsimpson05@gmail.com>
2018-05-22 16:48:37 -0400

    Collecting input()

---
+++ lab_input/input_ip.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#Illistrated by Lashawn Simpson
+
+#Collecting Input()
+
+## Collect Input for user
+user_input = input('Please enter an IPv4 addresss:')
+
+## Print out the input collected
+print("You told me the IPv4 address is:" + user_input)
