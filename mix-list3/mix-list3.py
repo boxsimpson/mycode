858ce7163cf8c0f761e74fa071e3685b170261ce
Box <lashawnjsimpson05@gmail.com>
2018-05-22 17:47:28 -0400

    List of List

---
+++ mix-list3/mix-list3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#Illustrated by Lashawn Simpson
+
+list1 = ['cisco_nxos', 'artista_eos', 'cisco_ios']
+print(list1)
+list1.extend(['juniper'])
+print(list1)
+list1.append(['10.1.0.1', '10.2.0.1', '10.3.0.1'])
+print(list1)
+print(list1[4])
+print(list1[4][0])
