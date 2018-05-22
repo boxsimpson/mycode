205154ef46eeb546cbd295e075db38fe601c3e4e
Box <lashawnjsimpson05@gmail.com>
2018-05-22 11:37:02 -0400

    Day 2

---
+++ day2/orange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Illistrated by Lashawn Simpson
+#Print ORANGE by using six(6) print() functions
+
+print('O', end='')
+print('R', end='')
+print('A', end='')
+print('N', end='')
+print('G', end='')
+print('E', end='')
