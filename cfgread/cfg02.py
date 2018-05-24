404104a4b75e2ad8e4d2eb659b807ec972d9fdf1
Box <lashawnjsimpson05@gmail.com>
2018-05-24 15:28:11 -0400

    Reading from a file

---
+++ cfgread/cfg02.py
@@ -0,0 +1,16 @@
+#/usr/bin/env python3
+## create file object in "r"ead mode
+configfile = open('vlanconfig.cfg', 'r')
+
+## display file to the screen - .read()
+print(configfile.read())
+
+## make a list of file lines - .readlines()
+print(config.readlines())
+configlist = config.readlines()
+
+for x in configlist:
+    print(x)
+
+## Always close your file
+configfile.close()
